0da5e9cbc3e26020c61b0dcbdab4f34ed675662e
Colby Kauk <91248100+s3mi0tics@users.noreply.github.com>
2022-12-31 15:38:47 -0800

    Delete src directory

--- src/components/NotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotAvailable() {
-  return (
-    <h1 className="not-available">
-      No Movies Available for selected genre
-    </h1>
-  )
-}
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "b25596773eb1c57f41e14fb015a8ac74";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3"
